97b7329a4a90a102964e03e773733b02ec404a05
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-04-14 18:56:56 +0200

    adding all components to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { PrimeNG } from 'primeng/config';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent,ProductComponent,CategoryComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'helloAngular';
+  constructor(private primeng: PrimeNG) {}
+  ngOnInit() {
+    this.primeng.ripple.set(true);
+}
 }
